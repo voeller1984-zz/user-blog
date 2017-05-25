6c02425ae53566e880c2939c4ad708e39bc29ad9
shahin <shahin@shahins-MacBook-Pro.local>
2017-05-25 23:51:56 +0200

    initial submission

---
+++ post.py
@@ -0,0 +1,19 @@
+from google.appengine.ext import db
+from user import User
+import helper
+
+
+class Post(db.Model):
+    user_id = db.IntegerProperty(required=True)
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+
+    def getUserName(self):
+        user = User.by_id(self.user_id)
+        return user.name
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return helper.jinja_render_str("post.html", p=self)
---
+++ user.py
@@ -0,0 +1,60 @@
+import random
+import hashlib
+
+from string import letters
+from google.appengine.ext import db
+
+
+# Helper functions
+def make_salt(length=5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+
+def make_pw_hash(name, pw, salt=None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (salt, h)
+
+
+def valid_pw(name, password, h):
+    salt = h.split(',')[0]
+    return h == make_pw_hash(name, password, salt)
+
+
+def users_key(group='default'):
+    return db.Key.from_path('users', group)
+
+
+# User Model
+class User(db.Model):
+
+    name = db.StringProperty(required=True)
+    pw_hash = db.StringProperty(required=True)
+    email = db.StringProperty()
+
+    @classmethod
+    def by_id(self, uid):
+        #    fetch User object from database
+        return User.get_by_id(uid, parent=users_key())
+
+    @classmethod
+    def by_name(self, name):
+        # get List of User objects from database.
+        u = User.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(self, name, pw, email=None):
+        #create a new user in db
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent=users_key(),
+                    name=name,
+                    pw_hash=pw_hash,
+                    email=email)
+
+    @classmethod
+    def login(self, name, pw):
+        u = self.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
